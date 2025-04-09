268279b8f09b18974ae5bc3b9945d17a71be242b
Radha Kotra <kradha6113@gmail.com>
2025-04-09 16:16:04 -0400

    Add DAO methods for inserting plants and photos, and getting plant data

--- app/src/main/java/com/example/growth/database/PlantDao.kt
+++ app/src/main/java/com/example/growth/database/PlantDao.kt
@@ -1,2 +1,23 @@
 package com.example.growth.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.growth.model.PlantPhoto
+
+@Dao
+interface PlantDao {
+    @Insert
+    suspend fun insertPlant(plant: Plant): Long
+
+    @Insert
+    suspend fun insertPlantPhoto(photo: PlantPhoto)
+
+    @Query("SELECT * FROM Plant")
+    suspend fun getAllPlants(): List<Plant>
+
+    @Query("SELECT * FROM PlantPhoto WHERE plantId = :plantId ORDER BY dateTaken ASC")
+    suspend fun getPhotosForPlant(plantId: Int): List<PlantPhoto>
+
+    // Add other queries
+}
